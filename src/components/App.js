a1f524cbb29af7d8e56ecadb38f9fdc65998db4c
Luis Londono <Luis.Londono@neuanalytics.com>
2020-07-17 16:12:10 -0500

    update comment

--- src/components/App.js
+++ src/components/App.js
@@ -68,13 +68,13 @@ class App extends React.Component {
     }
 
     addToOrder = (key) => {
-        // 1. Take a copy of state
+        // 1 Take a copy of state
         const order = { ...this.state.order };
 
-        // 2. Eithier add to the order, or update the number in our order
+        // 2 Eithier add to the order, or update the number in our order
         order[key] = order[key] + 1 || 1;
 
-        // 3. Call setState to update our state object
+        // 3 Call setState to update our state object
         this.setState({ order });
     }
 
